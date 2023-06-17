b507361e42c1eb584db1bb82df01054294ea01be
Jotace011 <jotace.026.3@gmail.com>
2023-06-16 23:15:08 -0500

    Agregando Funcion Login con Firebase

--- PROYECTO RESTAURANTE/LoginFormViewController.swift
+++ PROYECTO RESTAURANTE/LoginFormViewController.swift
@@ -1,8 +1,13 @@
 import UIKit
+import FirebaseAuth
 
 class LoginFormViewController: UIViewController {
 
     @IBOutlet weak var buttonEntrar: UIButton!
+    
+    @IBOutlet weak var textCorreo: UITextField!
+    
+    @IBOutlet weak var textClave: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -12,17 +17,31 @@ class LoginFormViewController: UIViewController {
     }
     
     @IBAction func verificarLogin(_ sender: Any) {
-        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        let viewController = storyBoard.instantiateViewController(withIdentifier: "MenuTabBar") as! MenuTabBarController
-        
-        viewController.modalPresentationStyle = .fullScreen
-        self.present(viewController, animated: true, completion: nil)
-        
+       let correo = textCorreo.text!
+        let clave = textClave.text!
+       
+        Auth.auth().signIn(withEmail: correo, password: clave){ authResult, error in
+            if let user = authResult {
+                //login correcto
+                print(user.user.uid)
+                self.goToMain()
+            }else{
+                //login error
+            }
+            
+        }
         
     }
     
-    
-    
+     func goToMain(){
+         let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+               let viewController = storyBoard.instantiateViewController(withIdentifier: "MenuTabBar") as! MenuTabBarController
+               
+               viewController.modalPresentationStyle = .fullScreen
+               self.present(viewController, animated: true, completion: nil)
+        
+    }
+
     
     
     
